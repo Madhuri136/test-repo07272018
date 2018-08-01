7fa90a1382b3f7169eff9f48e8b3890f1d3fdb54
Madhuri136 <41760675+Madhuri136@users.noreply.github.com>
2018-08-01 14:39:03 -0400

    Add files via upload
    My test IOC

---
+++ NormalCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+
+    public class NormalCustomer:ICustomer
+    {
+        public string GetName()
+        {
+            return "I am a Normal Customer";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Container;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+            container.RegisterType <ICustomer, NormalCustomer>();
+            //NormalCustomer customer = new NormalCustomer();
+           var customer= container.Resolve<ICustomer>();
+            customer.GetName();
+            Console.WriteLine("hi");
+            Console.WriteLine(customer.GetName());
+            PrefferedCustomer pcustomer = new PrefferedCustomer();
+            Console.WriteLine(pcustomer.GetName());
+            Console.ReadLine();
+        }
+    }
+}
+         
